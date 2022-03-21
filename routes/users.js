7bff513227c0a6c3f2c04054c16081e461ba1042
ArtJSON <artur.pietrzak1@outlook.com>
2022-03-21 19:30:13 +0100

    Add better handling of user registration errors

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 const router = express.Router({mergeParams: true});
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    req.flash('success', 'Created new account');
-    res.redirect('/racetracks');
-});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Created new account');
+        res.redirect('/racetracks');
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/register');
+    }
+}));
 
 module.exports = router;
