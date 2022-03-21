70b6121cc17122ca0e764187285196432d3a9b74
ArtJSON <artur.pietrzak1@outlook.com>
2022-03-22 00:11:07 +0100

    Add author to racetrack model and seeds

--- models/racetrack.js
+++ models/racetrack.js
@@ -11,6 +11,10 @@ const RacetrackSchema = new Schema({
     },
     description: String,
     location: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in to perform this action');
         return res.redirect('/login');
     }
