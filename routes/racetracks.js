7ce215855ffde09ddc13312d48d9ab0093b899fd
ArtJSON <artur.pietrzak1@outlook.com>
2022-03-23 02:00:51 +0100

    create middleware to check if user is author of the race track in order to modify it

--- routes/racetracks.js
+++ routes/racetracks.js
@@ -18,6 +18,16 @@ const validateRaceTrack = (req, res, next) => {
     }
 };
 
+const isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const racetrack = await Racetrack.findById(id);
+    if (!racetrack.author.equals(req.user_id)) {
+        req.flash('error', 'You do not have permission to do that');
+        return res.redirect(`/racetracks/${id}`);
+    }
+    next();
+};
+
 router.get('/', async (req, res) => {
     const racetracks = await Racetrack.find({});
     res.render('racetracks/index', { racetracks });
@@ -27,7 +37,7 @@ router.get('/new', isLoggedIn, (req, res) => {
     res.render('racetracks/new');
 });
 
-router.post('/', [isLoggedIn, validateRaceTrack], catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateRaceTrack, catchAsync(async (req, res, next) => {
     const newRacetrack = new Racetrack(req.body.racetrack);
     newRacetrack.author = req.user._id;
     newRacetrack.save();
@@ -35,14 +45,14 @@ router.post('/', [isLoggedIn, validateRaceTrack], catchAsync(async (req, res, ne
     return res.redirect(`/racetracks/${newRacetrack.id}`);
 }));
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res, next) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
     const {id} = req.params;
     await Racetrack.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted the race track');
     res.redirect('/racetracks');
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res, next) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
     const racetrack = await Racetrack.findById(req.params.id);
     if (!racetrack) {
         req.flash('error', 'Cannot find this race track');
@@ -52,7 +62,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res, next) => {
 }));
 
 router.get('/:id', catchAsync(async (req, res, next) => {
-    const id = req.params.id;
+    const { id } = req.params;
     const racetrack = await Racetrack.findById(id).populate('reviews').populate('author'); 
     if (!racetrack) {
         req.flash('error', 'Cannot find this race track');
@@ -61,14 +71,10 @@ router.get('/:id', catchAsync(async (req, res, next) => {
     res.render('racetracks/show', { racetrack });
 }));
 
-router.patch('/:id', [isLoggedIn, validateRaceTrack], catchAsync(async (req, res, next) => {
+router.patch('/:id', isLoggedIn, isAuthor, validateRaceTrack, catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const racetrack = await Racetrack.findById(id);
-    if (!racetrack.author.equals(req.user_id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/racetracks/${id}`);
-    }
-    const track = await Racetrack.findById(id, req.body.racetrack);
+
+    const racetrack = await Racetrack.findById(id, req.body.racetrack);
     req.flash('success', 'Successfully updated race track data');
     res.redirect(`/racetracks/${racetrack.id}`)
 }));
