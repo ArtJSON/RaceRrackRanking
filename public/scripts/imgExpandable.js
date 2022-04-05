4281c4af3b9c944fefa63f52ad27688fdc767d99
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-05 02:09:16 +0200

    Display full size img after clicking on img-expandable

--- public/scripts/imgExpandable.js
+++ public/scripts/imgExpandable.js
@@ -2,6 +2,13 @@ const imgs = document.querySelectorAll(".img-expandable");
 
 imgs.forEach((img) => {
   img.addEventListener("click", function () {
-    console.log("hello");
+    let fullImg = document.createElement("img");
+    fullImg.src =
+      "https://res.cloudinary.com/dixadtjff/image/upload/c_fill,h_600,w_800/v1648921468/racetracks/ylkhk9fhkuf97oacilw2.jpg".replace(
+        /(?<=upload).(?:(?!\/).)*\//g,
+        "/"
+      );
+    fullImg.classList.add("img-expanded");
+    img.parentElement.parentElement.parentElement.append(fullImg);
   });
 });
