b218f7fc58e2ed679f26ff7c23216754ccdf9968
ArtJSON <artur.pietrzak1@outlook.com>
2022-04-14 00:01:22 +0200

    Add boilerplate (navbar and separate site) for admin controls

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", authAdmin, async (req, res) => {
-  res.send("dasdasdsadasd");
+  res.render("admin/index");
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const User = require('../models/user');
-const catchAsync = require('../utils/catchAsync');
-const router = express.Router({mergeParams: true});
-const passport = require('passport');
-const userController = require('../controllers/users');
-const Racetrack = require('../models/racetrack')
+const express = require("express");
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+const router = express.Router({ mergeParams: true });
+const passport = require("passport");
+const userController = require("../controllers/users");
+const Racetrack = require("../models/racetrack");
 
-const mapboxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapboxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapboxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mapboxGeocoding({accessToken: mapboxToken});
+const geocoder = mapboxGeocoding({ accessToken: mapboxToken });
 
-router.route('/register')
-    .get(userController.showRegisterForm)
-    .post(catchAsync(userController.register));
+router
+  .route("/register")
+  .get(userController.showRegisterForm)
+  .post(catchAsync(userController.register));
 
-router.route('/login')
-    .get(userController.showLoginForm)
-    .post(passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), userController.logIn);
+router
+  .route("/login")
+  .get(userController.showLoginForm)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    userController.logIn
+  );
 
-router.get('/logout', userController.logout);
+router.get("/logout", userController.logout);
 
 module.exports = router;
